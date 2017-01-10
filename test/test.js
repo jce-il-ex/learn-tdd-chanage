8d00fa149cff3e4f9f0001368bebb2ba19b45c29
Tmura <Tmura>
2017-01-10 18:58:41 +0200

    RED:a failing test

--- test/test.js
+++ test/test.js
@@ -12,3 +12,6 @@ describe('Cash Register', function(){
   })
 }); 
 var C = require('../cash.js');  // our module
+it('getChange(210,300) should equal [50,20,20]', function(){
+    assert.deepEqual(C.getChange(210,300), [50,20,20]);
+})
